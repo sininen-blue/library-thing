76262459fb02ae2213eca90b59045687f1b2b165
sininen-blue <108949710+sininen-blue@users.noreply.github.com>
2024-12-04 23:49:02 +0800

    base

--- library/shelves/models.py
+++ library/shelves/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Shelf(models.Model):
+    title = models.CharField(max_length=200)
+
+
+class Book(models.Model):
+    shelf = models.ForeignKey(Shelf, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
