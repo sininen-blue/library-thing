ce13dd6efc6ec8f3a63221e13a63b4eefc47a5ee
sininen-blue <108949710+sininen-blue@users.noreply.github.com>
2024-12-05 19:20:59 +0800

    base html

--- library/shelves/urls.py
+++ library/shelves/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = "shelves"
 urlpatterns = [
     path("", views.index, name="index"),
+    path("lib/<str:lib_name>", views.library, name="library"),
+    path("lib/<str:lib_name>/<int:shelf>", views.shelf, name="shelf"),
+    path("lib/<str:lib_name>/<int:shelf>/<int:row>", views.row, name="row"),
 ]
--- library/shelves/views.py
+++ library/shelves/views.py
@@ -2,4 +2,16 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, "shelves/base.html")
+    return render(request, "shelves/index.html")
+
+
+def library(request, lib_name):
+    return render(request, "shelves/library.html")
+
+
+def shelf(request, lib_name, shelf):
+    return render(request, "shelves/shelf.html")
+
+
+def row(request, lib_name, shelf, row):
+    return render(request, "shelves/shelf.html")
