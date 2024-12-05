f5482a83d9fdedc0b9ce8f67951588e15790627f
sininen-blue <108949710+sininen-blue@users.noreply.github.com>
2024-12-05 20:01:59 +0800

    updated models

--- library/shelves/admin.py
+++ library/shelves/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Book, Shelf
+from .models import Library, Book, Shelf
 
+admin.site.register(Library)
 admin.site.register(Book)
 admin.site.register(Shelf)
--- library/shelves/models.py
+++ library/shelves/models.py
@@ -4,12 +4,19 @@ from django.db import models
 class Library(models.Model):
     title = models.CharField(max_length=50, default="")
 
+    def __str__(self):
+        return f"{self.title}"
+
 
 class Shelf(models.Model):
     library = models.ForeignKey(Library, on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
+    number = models.IntegerField(default=0)
+    title = models.CharField(max_length=200, default="")
 
 
 class Book(models.Model):
     shelf = models.ForeignKey(Shelf, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    call_number = models.CharField(max_length=200)
+    barcode = models.CharField(max_length=200)
